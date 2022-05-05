22d92ae13caafd11e0814706fd3c3e19e7119f41
NasseriYa <webme724@gmail.com>
2022-05-05 17:11:55 +0430

    1.add bootstrap css and sweetAlert plugin to project/ 2.add empty main component:UserList to root dir

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserListComponent from './UserList.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserListComponent />
   </React.StrictMode>
 );
 
