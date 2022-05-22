acf916b37bd37e62931273d15d31a98c100f908f
fatemeh-pooladrag-developer <fa.pooladrag@gmail.com>
2022-05-22 13:37:13 +0430

    Fix:Fetch data for data table

--- src/Helper/apiAddressesFunction.jsx
+++ src/Helper/apiAddressesFunction.jsx
@@ -1,25 +1,27 @@
 import axios from "axios";
 
 export const ApiAddresses = () => {
-    return 'https://6283f9ca6b6c317d5ba763ab.endapi.io/usersList';
-}
+  return "https://6283f9ca6b6c317d5ba763ab.endapi.io/usersList";
+};
 
 // export let AxiosGet = async()=> {
-export let AxiosGet = ()=> {
-    let responseAxios = {
-        isData: false,
-        loading: true,
-        data: [],
-        error: [],
+export let AxiosGet = async () => {
+  let responseAxios = {
+    isData: false,
+    loading: true,
+    data: [],
+    error: [],
+  };
+  try {
+    const response = await axios.get(ApiAddresses());
+    if (response.data.data.length > 0) {
+      responseAxios.isData = true;
+      responseAxios.data = response.data.data;
+      responseAxios.loading = false;
     }
-    axios.get(ApiAddresses()).then((response) => {
-        if (response.data.data.length > 0) {
-            responseAxios.isData = true;
-            responseAxios.data = response.data.data;
-            responseAxios.loading = false;
-        }
-    }).catch(error => {
-        responseAxios.error = error;
-    })
-    return responseAxios;
-}
+  } catch (error) {
+    responseAxios.error = error;
+  }
+
+  return responseAxios;
+};
